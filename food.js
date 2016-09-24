9e1e107a16ea2f8ec37b47ba14cd34a3e008d0cd
david <david.lucky.star@gmail.com>
2016-09-24 17:26:34 +0300

    working foody

--- food.js
+++ food.js
@@ -4,33 +4,38 @@ app.controller('ctrl', function($scope, $http) {
     $scope.endpoints = ['http://localhost:8080', 'http://91.240.84.2:8080', 'http://192.168.10.22:8080', 'http://192.168.10.21:8080'];
 
     $scope.putFoody = function() {
-      // var request = {
-      //   "name": $scope.foodname,
-      //   "person": $scope.foodperson
-      // };
-      // if ($scope.fooddate) {
-      //   request.date = $scope.fooddate;
-      // };
-      // if ($scope.foodweight) {
-      //   request.weight = $scope.foodweight;
-      // }
-      var params = '?name=' + $scope.foodname + '&person=' + $scope.foodperson;
+      var request = {
+        "name": $scope.foodname,
+        "person": $scope.foodperson
+      };
       if ($scope.fooddate) {
-        params = params + '&date=' + $scope.fooddate;
-      }
+        request.date = $scope.fooddate;
+      };
       if ($scope.foodweight) {
-        params = params + '&weight=' + $scope.foodweight;
+        request.weight = $scope.foodweight;
       }
-      $http.get($scope.endpoint + '/foody' + params).then(function(res) {
+      // var params = '?name=' + $scope.foodname + '&person=' + $scope.foodperson;
+      // if ($scope.fooddate) {
+      //   params = params + '&date=' + $scope.fooddate;
+      // }
+      // if ($scope.foodweight) {
+      //   params = params + '&weight=' + $scope.foodweight;
+      // }
+      // $http.get($scope.endpoint + '/foody' + params).then(function(res) {
+      //   console.log(res);
+      //   $scope.refreshFoodies();
+      // });
+      var prefix = $scope.endpoint ? $scope.endpoint : '';
+      $http.put(prefix + '/foody', request).then(function(res) {
         console.log(res);
         $scope.refreshFoodies();
       });
     }
 
     $scope.refreshFoodies = function() {
-      $http.get($scope.endpoint + '/foodies').then(function(res) {
-        var response = JSON.parse(res.responseText);
-        $scope.foodies = response;
+      var prefix = $scope.endpoint ? $scope.endpoint : '';
+      $http.get(prefix + '/foodies').then(function(res) {
+        $scope.foodies = res.data;
       })
     }
 
